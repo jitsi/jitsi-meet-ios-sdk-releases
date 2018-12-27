000983cdb7117b5459af939fe2046e451c03ea3e
Saúl Ibarra Corretgé <s@saghul.net>
2018-12-27 15:59:32 +0100

    Add changelog

--- release.sh
+++ release.sh
@@ -34,5 +34,6 @@ git tag -a -m "${VERSION}" ${VERSION}
 echo "Done!"
 echo "You can now push all changes to git by doing: git push && git push --tags"
 echo "The new pod can be pushed to CocoaPods by doing: pod trunk push JitsiMeetSDK.podspec"
+echo "Don't forget to update the changelog!"
 
 exit 0
