5db9cb8bf0c649cbcb716fceafb0de5f95e5ecd8
Saúl Ibarra Corretgé <s@saghul.net>
2018-12-27 15:45:28 +0100

    1.21.1

---
+++ JitsiMeetSDK.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |s|
+  s.name             = 'JitsiMeetSDK'
+  s.version          = '1.21.1'
+  s.summary          = 'Jitsi Meet iOS SDK'
+  s.description      = 'Jitsi Meet is a WebRTC compatible, free and Open Source video conferencing system that provides browsers and mobile applications with Real Time Communications capabilities.'
+  s.homepage         = 'https://jitsi.org'
+  s.license          = { :type => 'Apache 2', :file => 'jitsi-meet-ios-sdk-releases-1.21.0/LICENSE' }
+  s.authors          = 'The Jitsi Meet project authors'
+  s.source           = { :http => 'https://github.com/jitsi/jitsi-meet-ios-sdk-releases/archive/1.21.1.zip' }
+  s.social_media_url = 'https://twitter.com/jitsinews'
+
+  s.platform         = :ios, '10.0'
+
+  s.vendored_frameworks = 'jitsi-meet-ios-sdk-releases-1.21.1/Frameworks/JitsiMeet.framework', 'jitsi-meet-ios-sdk-releases-1.21.1/Frameworks/WebRTC.framework'
+end
---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: release.sh VERSION"
+    exit 1;
+fi
+
+VERSION=$1
+
+# Generate podspec file
+cat JitsiMeetSDK.podspec.tpl | sed -e s/VERSION/${VERSION}/g > JitsiMeetSDK.podspec
+
+# Cleanup
+rm -rf Frameworks/*
+
+# Build the SDK
+pushd ../jitsi-meet
+rm -rf ios/sdk/JitsiMeet.framework
+xcodebuild -workspace ios/jitsi-meet.xcworkspace -scheme JitsiMeet -destination='generic/platform=iOS' -configuration Release archive
+popd
+
+# Put the new files in the repo
+cp -r ../jitsi-meet/ios/sdk/JitsiMeet.framework Frameworks/
+cp -r ../jitsi-meet/node_modules/react-native-webrtc/ios/WebRTC.framework Frameworks/
+
+# Add all files to git
+git add -A
+git commit -a -m "${VERSION}"
+git tag -a -m "${VERSION}" ${VERSION}
+
+echo "Done!"
+echo "You can now push all changes to git by doing: git push && git push --tags"
+echo "The new pod can be pushed to CocoaPods by doing: pod trunk push JitsiMeetSDK.podspec"
+
+exit 0
