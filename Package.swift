f85227c6ed6ec4da81dca5c16ec7810d204e6001
Valentin Wallet <valentinwallet@Valentins-MacBook-Pro.local>
2024-04-25 17:58:44 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "JitsiMeetSDK",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "JitsiMeetSDK",
+            targets: ["JitsiMeetSDKWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jitsi/webrtc", branch: "master"),
+        .package(url: "https://github.com/Giphy/giphy-ios-sdk", from: "2.2.9")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "JitsiMeetSDK",
+            path: "Frameworks/JitsiMeetSDK.xcframework"
+        ),
+        .target(
+            name: "JitsiMeetSDKWrapper",
+            dependencies: [
+                .target(name: "JitsiMeetSDK"),
+                .product(name: "WebRTC", package: "webrtc"),
+                .product(name: "GiphyUISDK", package: "giphy-ios-sdk")
+            ],
+            path: "Sources"
+        )
+    ]
+)
