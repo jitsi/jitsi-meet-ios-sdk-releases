1732eb697be35c6fcf23904f6e00369d8253d032
Calinteodor <calin.chitu@8x8.com>
2024-09-16 19:19:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "JitsiMeetSDK",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "JitsiMeetSDK",
